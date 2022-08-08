adb5c0843246a9e3dd2496419c2902585c3a154b
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-08 23:59:10 +0500

    basic setup of routes for different catagories of news

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React, { Component } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import News from "./Components/News/News";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export default class App extends Component {
   render() {
     return (
       <>
-        <Navbar />
-        <News pageSize={6} country="us" category="science" />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={<News pageSize={6} country="us" category="general" />}
+            ></Route>{" "}
+            <Route
+              exact
+              path="/business"
+              element={<News pageSize={6} country="us" category="business" />}
+            ></Route>{" "}
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News pageSize={6} country="us" category="entertainment" />
+              }
+            ></Route>{" "}
+            <Route
+              exact
+              path="/general"
+              element={<News pageSize={6} country="us" category="general" />}
+            ></Route>{" "}
+            <Route
+              exact
+              path="/health"
+              element={<News pageSize={6} country="us" category="health" />}
+            ></Route>{" "}
+            <Route
+              exact
+              path="/science"
+              element={<News pageSize={6} country="us" category="science" />}
+            ></Route>{" "}
+            <Route
+              exact
+              path="/sports"
+              element={<News pageSize={6} country="us" category="sports" />}
+            ></Route>{" "}
+            <Route
+              exact
+              path="/technology"
+              element={<News pageSize={6} country="us" category="technology" />}
+            ></Route>
+          </Routes>
+        </Router>
       </>
     );
   }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 export class Navbar extends Component {
   render() {
     return (
       <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             NewsApp
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -23,19 +23,19 @@ export class Navbar extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="/">
+                <Link className="nav-link" to="/">
                   Home <span className="sr-only">(current)</span>
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link className="nav-link" to="/">
                   Link
-                </a>
+                </Link>
               </li>
               <li className="nav-item dropdown">
-                <a
+                <Link
                   className="nav-link dropdown-toggle"
-                  href="/"
+                  to="/"
                   id="navbarDropdown"
                   role="button"
                   data-toggle="dropdown"
@@ -43,18 +43,29 @@ export class Navbar extends Component {
                   aria-expanded="false"
                 >
                   Dropdown
-                </a>
+                </Link>
                 <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="/">
-                    Action
-                  </a>
-                  <a className="dropdown-item" href="/">
-                    Another action
-                  </a>
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-item" href="/">
-                    Something else here
-                  </a>
+                  <Link className="dropdown-item" to="/business">
+                    Business
+                  </Link>
+                  <Link className="dropdown-item" to="/entertainment">
+                    Entertainment
+                  </Link>
+                  <Link className="dropdown-item" to="/general">
+                    General
+                  </Link>
+                  <Link className="dropdown-item" to="/health">
+                    Health
+                  </Link>
+                  <Link className="dropdown-item" to="/sports">
+                    Sports
+                  </Link>
+                  <Link className="dropdown-item" to="/science">
+                    Science
+                  </Link>
+                  <Link className="dropdown-item" to="/technology">
+                    Technology
+                  </Link>
                 </div>
               </li>
             </ul>
