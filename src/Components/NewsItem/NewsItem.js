5953a34c7f45d57eb0728a935d05f596aa56f5b8
Muhammad Abdullah <mafivs51@gmail.com>
2022-08-12 01:05:31 +0500

    fixing source badge

--- src/Components/NewsItem/NewsItem.js
+++ src/Components/NewsItem/NewsItem.js
@@ -6,18 +6,17 @@ export class NewsItem extends Component {
       this.props;
     return (
       <div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "flex-end",
-            position: "absolute",
-            right: "0",
-            zIndex: 1,
-          }}
-        >
-          <span className="badge rounded-pill bg-info"> {source} </span>
-        </div>{" "}
         <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge bg-info"> {source} </span>
+          </div>
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
